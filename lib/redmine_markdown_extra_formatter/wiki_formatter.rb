62aca7283f4a816e43b5db6a1a37e799ceda37ff
Kohei Yamazaki <yamazaki@fujisan.co.jp>
2012-10-12 12:45:44 +0900

    enable section editing

--- lib/redmine_markdown_extra_formatter/wiki_formatter.rb
+++ lib/redmine_markdown_extra_formatter/wiki_formatter.rb
@@ -146,5 +146,67 @@ module RedmineMarkdownExtraFormatter
         "</code></pre>"
       }
     end
+
+    def get_section(index)
+      section = extract_sections(index)[1]
+      hash = Digest::MD5.hexdigest(section)
+      return section, hash
+    end
+
+    def update_section(index, update, hash=nil)
+      t = extract_sections(index)
+      if hash.present? && hash != Digest::MD5.hexdigest(t[1])
+        raise Redmine::WikiFormatting::StaleSectionError
+      end
+      t[1] = update unless t[1].blank?
+      t.reject(&:blank?).join "\n\n"
+    end
+
+    private
+
+    def extract_sections(index)
+      selected, before, after = [[],[],[]]
+      pre = :none
+      state = 'before'
+
+      selected_level = 0
+      header_count = 0
+
+      @text.each_line do |line|
+
+        if line =~ /^(~~~|```)/
+          pre = pre == :pre ? :none : :pre
+        elsif pre == :none
+          
+          level = if line =~ /^(#+)/
+                    $1.length
+                  elsif line.strip =~ /^=+$/ 
+                    line = eval(state).pop + line
+                    1
+                  elsif line.strip =~ /^-+$/ 
+                    line = eval(state).pop + line
+                    2
+                  else
+                    nil
+                  end
+          unless level.nil?
+            if level <= 4
+              header_count += 1
+              if state == 'selected' and selected_level >= level
+                state = 'after'
+              elsif header_count == index
+                state = 'selected'
+                selected_level = level
+              end
+            end
+          end
+        end
+
+        eval("#{state} << line")
+      end
+
+      [before, selected, after].map{|x| x.join.strip}
+    end
+
   end
 end
